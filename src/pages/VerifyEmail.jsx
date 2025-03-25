fd6c2bcfac4595fa3433cc785412a987c3b95d35
Piyush Goyal <piyush2022ug@gmail.com>
2025-03-25 21:18:50 +0530

    feat: resetPassword page

--- src/pages/VerifyEmail.jsx
+++ src/pages/VerifyEmail.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { useLocation, Link, useNavigate } from 'react-router-dom';
 
 export default function VerifyEmail() {
+  const [verificationStatus, setVerificationStatus] = useState({
+    loading: false,
+    success: false,
+    error: null,
+  })
+
   return (
     <div>VerifyEmail</div>
   )
